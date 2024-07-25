00ca9126e4e1190881585bf11ca274a87560ad7f
Jomagene <semjomagene@gmail.com>
2024-07-25 23:44:31 +0200

    fix: Amelioration of the added websites style

--- components/SelectSite.tsx
+++ components/SelectSite.tsx
@@ -8,8 +8,9 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Input } from "./ui/input";
-import Image from "next/image";
 import websites from "@/public/websites";
+import { Equal, Link as LinkIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface SelectSiteProps {
   update: React.Dispatch<React.SetStateAction<string[]>>;
@@ -30,38 +31,49 @@ const SelectSite: FC<SelectSiteProps> = ({ list, update }) => {
   };
 
   return (
-    <div className="flex flex-col w-full rounded-[12px] h-[228px] bg-[#FAFAFA] p-5">
-      <Select onValueChange={handleChange}>
-        <SelectTrigger className="h-12 rounded-md text-sm w-full transition focus:border-none focus:!outline-[#633CFF] focus:shadow-shadowInput pl-7">
-          <SelectValue placeholder="Select a website" />
-        </SelectTrigger>
-        <SelectContent className="p-3 w-[95%]">
-          {websites.map((website, id) => (
-            <SelectItem
-              key={id}
-              value={website.value}
-              className="pb-3 hover:!bg-[#333] h-12 border-b">
-              {website.name}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <form className="relative" onSubmit={handleSubmit}>
-        <Input
-          placeholder={
-            websites.filter((website) => website.value === selected)[0]
-              ?.placeholder
-          }
-          className="text-sm focus:border-none focus:!outline-[#633CFF] focus:shadow-shadowInput pl-7"
-        />
-        <Image
-          src="/icons/enveloppe.svg"
-          width={16}
-          height={16}
-          alt="email input"
-          className="top-9 left-2 absolute"
-        />
-      </form>
+    <div className="flex flex-col w-full rounded-[12px] h-[228px] bg-[#FAFAFA] p-5 gap-3">
+      <div className="flex justify-between items-center">
+        <h1 className="font-bold text-[16px] text-[#737373] flex items-center">
+          <Equal className="h-[20px]" />
+          {`Link #$`}
+        </h1>
+        <Button className="text-[#737373] font-normal">Remove</Button>
+      </div>
+      <div className="flex flex-col gap-1">
+        <span className="text-[12px]">Platform</span>
+        <Select onValueChange={handleChange}>
+          <SelectTrigger className="h-12 rounded-md text-sm w-full transition focus:border-none focus:!outline-[#633CFF] focus:shadow-shadowInput pl-7">
+            <SelectValue placeholder="Select a website" />
+          </SelectTrigger>
+          <SelectContent className="p-3 w-[95%]">
+            {websites.map((website, id) => (
+              <SelectItem
+                key={id}
+                value={website.value}
+                className="pb-3 hover:!bg-[#333] h-12 border-b">
+                {website.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="flex flex-col gap-1 relative">
+        <span className="text-[12px]">Link</span>
+        <form onSubmit={handleSubmit}>
+          <Input
+            placeholder={
+              websites.filter((website) => website.value === selected)[0]
+                ?.placeholder
+            }
+            className="text-sm focus:border-none focus:!outline-[#633CFF] focus:shadow-shadowInput pl-7"
+          />
+          <LinkIcon
+            width={16}
+            height={16}
+            className="top-9 left-2 absolute text-[#737373]"
+          />
+        </form>
+      </div>
     </div>
   );
 };
